5ac67aea7911d94adb77291f9e09df87a8afe4c1
Priyanshu Gaurav <gauravpriyanshu3@gmail.com>
2020-12-20 10:07:27 +0530

    added new section

--- js/main.js
+++ js/main.js
@@ -21,4 +21,24 @@ $(document).ready(function(){
 		type:'image',
 		gallery:{enabled:true}
 	});
+	
+	
+	//OWL CAROUSEL
+	
+	$('.site-main .about-area .owl-carousel').owlCarousel({
+		loop:true,
+		autoplay:true,
+		dots:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			544:{
+				items:2
+			}
+		}
+	});
+	
+	
 });
+
