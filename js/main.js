ea12e3ff44abfc63343e7f669deb1112ad79675e
Priyanshu Gaurav <gauravpriyanshu3@gmail.com>
2020-12-28 00:53:01 +0530

    responsive now

--- js/main.js
+++ js/main.js
@@ -39,6 +39,25 @@ $(document).ready(function(){
 		}
 	});
 	
+
+	//	Sticky navigation menu
+	
+	let nav_offest_top=$(".header_area").height()+50;
+	
+	function navbarFixed(){
+		if($(".header_area").length){
+			$(window).scroll(function(){
+				let scroll=$(window).scrollTop();
+				if(scroll >= nav_offest_top){
+					$(".header_area .main-menu").addClass("navbar_fixed");
+				} else{
+					$(".header_area .main-menu").removeClass("navbar_fixed");
+				}
+			})
+		}
+	}
+	
+	navbarFixed();
 	
 });
 
